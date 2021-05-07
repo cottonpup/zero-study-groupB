a5247c79b7ce1362c95ae2cec77d2b658e4bd678
Emily <67526014+EmilyLee1206@users.noreply.github.com>
2021-05-08 08:56:16 +0900

    add a new todo app

---
+++ react-study/todo-app/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState, useRef, useCallback } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: '리액트의 기초 알아보기', checked: true },
+    { id: 2, text: '컴포넌트 스타일링해 보기', checked: true },
+    { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
+  ]);
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+        // id값이 같을 때는 새로운 객체 반환, 다를 때는 변화를 주지 않고 처음 받아왔던 상태 그대로 반환
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
+  // 고윳값으로 사용될 id
+  // ref로 변수 담기
+  const nextId = useRef(4);
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
+};
+
+export default App;
---
+++ react-study/todo-app/src/components/TodoList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import TodoListItem from './TodoListItem';
+import './TodoList.scss';
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+  return (
+    <div className="TodoList">
+      {todos.map((todo) => (
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+        // map을 사용하여 컴포넌트로 변환할 때는 key props를 전달해주어야 한다.
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
