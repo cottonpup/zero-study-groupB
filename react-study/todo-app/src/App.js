f0636568fab6266f5271ce8c96f0fc03898e7263
Emily <67526014+EmilyLee1206@users.noreply.github.com>
2021-05-08 09:20:28 +0900

    create the bulk of todos

--- react-study/todo-app/src/App.js
+++ react-study/todo-app/src/App.js
@@ -3,47 +3,46 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
 const App = () => {
-  const [todos, setTodos] = useState([
-    { id: 1, text: '리액트의 기초 알아보기', checked: true },
-    { id: 2, text: '컴포넌트 스타일링해 보기', checked: true },
-    { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
-  ]);
+  const [todos, setTodos] = useState(createBulkTodos);
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
+  const onToggle = useCallback((id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
         // id값이 같을 때는 새로운 객체 반환, 다를 때는 변화를 주지 않고 처음 받아왔던 상태 그대로 반환
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  }, []);
 
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos],
-  );
-
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
   // 고윳값으로 사용될 id
   // ref로 변수 담기
   const nextId = useRef(4);
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos],
-  );
+
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo));
+    nextId.current += 1;
+  }, []);
 
   return (
     <TodoTemplate>
