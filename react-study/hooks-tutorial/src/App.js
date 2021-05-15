b563f15392c2bb17577eb933e7bcfd7d494a4d5a
Emily <67526014+EmilyLee1206@users.noreply.github.com>
2021-05-15 16:27:50 +0900

    add useReducer

---
+++ react-study/hooks-tutorial/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter from './Counter';
+// import Counter from './Counter';
+// import Info from './Info';
+// import { useState } from 'react';
+
+function App() {
+  // const [visible, setVisible] = useState(false);
+  return <Counter />;
+}
+
+export default App;
