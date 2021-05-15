aea48c8c1d6cd3bc1484dd080f2ac3fb12524938
Emily <67526014+EmilyLee1206@users.noreply.github.com>
2021-05-15 17:11:56 +0900

    add useRef

--- react-study/hooks-tutorial/src/Average.js
+++ react-study/hooks-tutorial/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useCallback } from 'react';
+import React, { useState, useMemo, useCallback, useRef } from 'react';
 
 const getAverage = (numbers) => {
   console.log('평균값 계산중..');
@@ -10,6 +10,7 @@ const getAverage = (numbers) => {
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState('');
+  const inputEl = useRef(null);
 
   const onChange = useCallback((e) => {
     setNumber(e.target.value);
@@ -19,6 +20,7 @@ const Average = () => {
     const nextList = list.concat(parseInt(number));
     setList(nextList);
     setNumber('');
+    inputEl.current.focus();
   }, [number, list]);
 
   const avg = useMemo(() => getAverage(list), [list]);
