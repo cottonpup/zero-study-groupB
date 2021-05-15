4263e644fa49e492c1369e5832a032f6a4fcfc46
Emily <67526014+EmilyLee1206@users.noreply.github.com>
2021-05-15 16:35:23 +0900

    control input using useReducer

--- react-study/hooks-tutorial/src/App.js
+++ react-study/hooks-tutorial/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Counter from './Counter';
 // import Counter from './Counter';
-// import Info from './Info';
+// import Counter from './Counter';
+import Info from './Info';
 // import { useState } from 'react';
 
 function App() {
   // const [visible, setVisible] = useState(false);
-  return <Counter />;
+  return <Info />;
 }
 
 export default App;
