f0b267ae688722d9127c7b13b4618ca1525dcecc
cottonpup <younggyounglee1@gmail.com>
2021-07-17 18:07:37 +0900

    handle routing with React Router

--- react-study/news-viewer/src/components/NewsList.js
+++ react-study/news-viewer/src/components/NewsList.js
@@ -1,59 +1,59 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import NewsItem from './NewsItem';
-import axios from 'axios';
-import { useEffect } from 'react';
+import React, { useState } from "react";
+import styled from "styled-components";
+import NewsItem from "./NewsItem";
+import axios from "axios";
+import { useEffect } from "react";
 
 const NewsListBlock = styled.div`
-  box-sizing: border-box;
-  padding-bottom: 3rem;
-  width: 768px;
-  margin: 0 auto;
-  margin-top: 2rem;
-  @media screen and (max-width: 768px) {
-    width: 100%;
-    padding-left: 1rem;
-    padding-right: 1rem;
-  }
+    box-sizing: border-box;
+    padding-bottom: 3rem;
+    width: 768px;
+    margin: 0 auto;
+    margin-top: 2rem;
+    @media screen and (max-width: 768px) {
+        width: 100%;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f`
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, [category]);
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const query = category === "all" ? "" : `&category=${category}`;
+                const response = await axios.get(
+                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f`
+                );
+                setArticles(response.data.articles);
+            } catch (e) {
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, [category]);
 
-  // 대기중일 때
-  if (loading) {
-    return <NewsListBlock>대기중...</NewsListBlock>;
-  }
+    // 대기중일 때
+    if (loading) {
+        return <NewsListBlock>대기중...</NewsListBlock>;
+    }
 
-  if (!articles) {
-    return null;
-  }
+    if (!articles) {
+        return null;
+    }
 
-  return (
-    <NewsListBlock>
-      {articles.map((article) => (
-        <NewsItem key={article.url} article={article} />
-      ))}
-    </NewsListBlock>
-  );
+    return (
+        <NewsListBlock>
+            {articles.map((article) => (
+                <NewsItem key={article.url} article={article} />
+            ))}
+        </NewsListBlock>
+    );
 };
 
 export default NewsList;
